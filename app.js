688acc5ba387b41609af0f403b24ef5a856cdf8e
Andrew <34489250+AndrewPhilippov@users.noreply.github.com>
2019-05-20 19:05:49 +0300

    Update app.js

--- app.js
+++ app.js
@@ -4,16 +4,16 @@ const InitAffix = () => {
     let elTop = el.getBoundingClientRect().top;
     let elBottom = el.getBoundingClientRect().bottom;
     let footer = document.getElementsByTagName('footer');
-    let footerDimensions = footer[0].getBoundingClientRect();
-    let footerTop = footer[0].offsetTop;
     let footerHeight = footer[0].offsetHeight;
     let isScrolling;
+    let beforeFooter = footer[0].offsetTop;
 
         // Listen for scroll events
-        window.addEventListener('scroll', function ( event ) {
+        window.addEventListener('scroll', function ( e ) {
             let winHeight = window.innerHeight;
             let winY = window.scrollY;
-            window.clearTimeout( isScrolling );
+            let docHeight = document.body.scrollHeight;
+            let stopBottom = elBottom + winY + 40;
 
             // Clear our timeout throughout the scroll
             window.clearTimeout( isScrolling );
@@ -22,24 +22,52 @@ const InitAffix = () => {
             isScrolling = setTimeout(function() {
 
                 // Run the callback
-                console.log( 'Scrolling has stopped.' );
-                if(winY+elHeight - elBottom > 15){
-                    el.classList.add('aff');
-                } else {
-                    el.classList.remove('aff');
-                    if(el.classList.contains('scroll-up')){
-                        el.style.marginTop = + winY + 'px';
+                console.log('/--------------------/');
+                console.log('window.innerHeight: ', winHeight);
+                console.log('window.scrollY: ', winY);
+                console.log('elBottom: ', elBottom);
+                console.log('StopBottom - elBottom + winY: ', stopBottom); // To get the point to stop before footer *** 40px - for margin
+                console.log('beforeFooter: ', footer[0].offsetTop);
+                console.log('DocHeight: ', docHeight);
+                console.log('footerHeight: ', footer[0].getBoundingClientRect().height);
+                console.log('el.offsetTop: ', el.offsetTop);
+                console.log('/--------------------/');
+
+                if(winHeight >= elBottom){
+                    if(stopBottom >= footer[0].offsetTop){
+                        console.log('Lower');
+                        el.classList.add('stopBottom');
+                        el.style.marginTop = '1350px';
+                    } else {
+                        el.classList.remove('stopBottom');
+                        el.style.marginTop = 'initial';
+                        console.log('Higher')
                     }
+                } else {
+                    let isTopVisible = elTop > 15;
+                    let isBottomVisible = elBottom > winY + elBottom;
+                    let isUnderFooter = beforeFooter;
+
+                    if(isTopVisible){
+                        el.style.position = 'fixed';
+                        el.style.top = '150px'
+                    } else if (!isTopVisible && isBottomVisible){
+                        el.style.position = 'fixed';
+                        el.style.bottom = '15px'
+                    }
+
+
+                    // el.classList.remove('aff');
+                    // if(el.classList.contains('scroll-up')){
+                    //     el.style.position = 'absolute';
+                    //     el.style.top = winY + 'px';
+                    // } else {
+                    //     el.style.position = 'fixed';
+                    //     el.style.bottom = '0';
+                    // }
                 }
-                console.log('/------//------//------//------/');
-                console.log(winHeight - (winY + winHeight));
-                console.log('/------//------//------//------/');
-
-            }, 66);
-
+            }, 0);
         }, false);
-
-
 
 //     $("#priceScrollSpy").affix({
 //         offset: {
